7f123dd3cbc09bf5e554b31535d94f0f2bd73f1c
Mukesh19052002 <mukesh19052002@gmail.com>
2022-06-28 21:45:37 +0530

    1 file modified

--- counter.jsx
+++ counter.jsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0 
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
   };
 
   styles = {
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 20,
   };
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-warning m-2">
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* <span style={this.styles}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
