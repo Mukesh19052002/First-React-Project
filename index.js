2807771efd1e7b9077260d047053ebd3172cbea9
Mukesh19052002 <mukesh19052002@gmail.com>
2022-07-12 12:13:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import './style.css';
 import Counter from './counter';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <h1>Hello World</h1>;
+
 ReactDOM.render(<Counter />, document.getElementById('root'));
